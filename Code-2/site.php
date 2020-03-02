5941b95affce98f0f35382ddde08a969a0c0682b
mfarzamalam <farzualam9999@gmail.com>
2020-03-02 14:03:09 +0500

    Divisible by 3 ir 7

---
+++ Code-2/site.php
@@ -0,0 +1,12 @@
+<?php 
+
+    for ($i=1;$i<=100;$i++){      // printing value from 1 to 100  
+
+        if($i%3 !==0 && $i%7 !==0){  // applying the condition to hide all the value that are divisible by 3 or 7
+
+            echo "$i <br>";
+        
+        }
+    }
+
+?>
