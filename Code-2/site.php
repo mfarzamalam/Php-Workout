547e603569eac4efa5c938689fcbbe494a338636
mfarzamalam <farzualam9999@gmail.com>
2020-03-02 14:29:00 +0500

    Divisible by 3 or 7

--- Code-2/site.php
+++ Code-2/site.php
@@ -2,7 +2,7 @@
 
     for ($i=1;$i<=100;$i++){      // printing value from 1 to 100  
 
-        if($i%3 !==0 && $i%7 !==0){  // applying the condition to hide all the value that are divisible by 3 or 7
+        if($i%3!==0  &&  $i%7!==0){  // applying the condition to hide all the value that are divisible by 3 or 7
 
             echo "$i <br>";
         
