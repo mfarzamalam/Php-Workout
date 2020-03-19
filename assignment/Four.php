5c584806857c973447570316754451eaae0d1a0c
mfarzamalam <farzualam9999@gmail.com>
2020-03-19 16:56:38 +0500

    Completed 2 out of 3

---
+++ assignment/Four.php
@@ -0,0 +1,70 @@
+<?php
+
+
+
+    // Q1. Write a php program containing Human Class having name, father name, age, gender. Create object 
+    // of human and print all the properties of human.
+
+
+
+    class Human {
+        public $Name;
+        public $Fathername;
+        public $Age;
+        public $Gender;
+    
+    function __construct($name,$fathername,$age,$gender)
+        {
+            $this->Name = $name;
+            $this->Fathername = $fathername;
+            $this->Age = $age;
+            $this->Gender = $gender;
+        }
+    }
+
+    Echo"<h1>Print all the value of Human class </h1>";
+
+    $pehla_human = New Human("Aslam","Achariya","54","Male");
+
+    foreach ($pehla_human as $aam_admi){
+        echo $aam_admi."<br>";
+    }
+
+
+
+    //Q2. Create another Class Student that is the child class of human having special properties like course,
+    // fees, Enrollment no and also having a method that print all the details about the student like student 
+    // name, age, course etc.
+
+
+
+    class Student extends Human {
+        public $Course;
+        public $Fees;
+        public $Enrollment;
+
+    function __construct($name,$age,$course,$fees,$enrollment)
+        {
+            $this->Age = $age;
+            $this->Name = $name;
+            $this->Course = $course;
+            $this->Fees = $fees;
+            $this->Enrollment = $enrollment;
+        }
+    
+    function StudentDetails() {
+            echo "Student name is <i>{$this->Name}</i> <br>
+            Student Age is <i>{$this->Age}</i> <br>
+            Student Course is <i>{$this->Course}</i> <br>";
+        }
+        
+    }
+
+    Echo"<h1>Print Name , Age and Course </h1>";
+    
+    $pehla_student = New Student("Talib elim","umer","Php","2400","1130251");
+    $pehla_student->StudentDetails();
+
+    
+
+?>
