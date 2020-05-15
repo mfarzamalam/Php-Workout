0175c0e70aaa88d2698f0dcdc9481d4f06b28af9
mfarzamalam <farzualam9999@gmail.com>
2020-05-15 21:55:33 +0500

    added seperate header file

--- assignment/shopwatch/login.php
+++ assignment/shopwatch/login.php
@@ -23,44 +23,9 @@
         <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <header>
-        <!-- Header Start -->
-        <div class="header-area">
-            <div class="main-header header-sticky">
-                <div class="container-fluid">
-                    <div class="menu-wrapper">
-                        <!-- Logo -->
-                        <div class="logo">
-                            <a href="index.php"><img src="assets/img/logo/logo.png" alt=""></a>
-                        </div>
-                        <!-- Main-menu -->
-                        <div class="main-menu d-none d-lg-block">
-                            <nav>                                                
-                                <ul id="navigation">  
-                                    <li><a href="index.php">Home</a></li>
-                                    <li><a href="shop.php">shop</a></li>
-                                    <li><a href="contact.php">Contact</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <!-- Header Right -->
-                        <div class="header-right">
-                            <ul>
-                                <li> <a href="login.php"><span class="flaticon-user"></span></a></li>
-                                <li><a href="cart.php"><span class="flaticon-shopping-cart"></span></a> </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- Mobile Menu -->
-                    <div class="col-12">
-                        <div class="mobile_menu d-block d-lg-none"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Header End -->
-    </header>
-    
+
+    <?php include "header.php"; ?>
+        
     <main>
         <!-- Hero Area Start-->
         <div class="slider-area ">
